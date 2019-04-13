301e692f9afcafa8fc4ff2815b903e0f6c301a06
your name <you@example.com>
2019-04-13 10:56:38 -0400

    Added a form and ImageList

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -16,7 +16,8 @@ namespace WindowsFormsApp2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+           // Application.Run(new fieldWindow());   Setting the field later.
+            Application.Run(new deckWindow());
         }
     }
 }
---
+++ WindowsFormsApp2/deckWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class deckWindow : Form
+    {
+        public deckWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void deckWindow_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
